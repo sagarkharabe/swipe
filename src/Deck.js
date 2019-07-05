4e2234570ddff7a28f2bec93e9a19691cf8b047f
sagarkharbe <sagarkharabe97@gmail.com>
2019-07-05 21:41:11 +0530

    initial commit 2

---
+++ src/Deck.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { View, Animated } from "react-native";
+
+export default class Deck extends Component {
+  renderCards = () => {
+    return this.props.data.map(item => this.props.renderCard(item));
+  };
+  render() {
+    return <View>{this.renderCards()}</View>;
+  }
+}
